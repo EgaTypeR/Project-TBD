8da6f7c7e959f9e4ee01c6e41b364a2cde2ad590
EgaTypeR <110679888+EgaTypeR@users.noreply.github.com>
2023-06-08 09:35:32 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express()
 const port = 5000
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Ganti dengan domain Anda
+  res.setHeader('Access-Control-Allow-Origin', 'https://project-tbd-dn3b.vercel.app/'); // Ganti dengan domain Anda
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
